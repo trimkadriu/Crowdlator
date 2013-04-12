a55d05296a96cdf2bd246d8101b95dc351c0b373
Trim Kadriu <trimkadriu@users.noreply.github.com>
2013-04-12 03:18:13 +0200

    SoundCloud controls ui layout

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -4,7 +4,10 @@ class Test extends CI_Controller {
 
     function index(){
         $this->load->model("soundcloud_model");
-        echo $this->soundcloud_model->get_access_token();
+        //echo $this->soundcloud_model->get_access_token();
+        $data['client_id'] = $this->config->item("soundcloud_client_id");
+        $data['redirect_url'] = $this->config->item("soundcloud_redirect_url");
+        $this->load->view("test", $data);
     }
 
 }
