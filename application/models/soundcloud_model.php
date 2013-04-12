a692655f4b9618dc31c80085f477857b29f4c391
trimkadriu <jareck_8@hotmail.com>
2013-04-12 03:18:13 +0200

    SoundCloud controls ui layout

--- application/models/soundcloud_model.php
+++ application/models/soundcloud_model.php
@@ -16,9 +16,11 @@ class Soundcloud_model extends CI_Model {
         $sc_user = $this->config->item("soundcloud_email");
         $sc_pass = $this->config->item("soundcloud_password");
         //SoundCloud token request - Python script
-        $script_path = dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'libraries/soundcloud_get_token.py';
+        $ds = DIRECTORY_SEPARATOR;
+        $script_location = dirname(__FILE__).$ds.'..'.$ds.'libraries'.$ds.'Soundcloud_get_token.py';
+        $params = $sc_client_id." ".$sc_secret." ".$sc_user." ".$sc_pass;
         //Execute python script and save results
-        exec("python \"".$script_path."\" ".$sc_client_id." ".$sc_secret." ".$sc_user." ".$sc_pass, $result);
+        exec("python \"".$script_location."\" ".$params, $result);
         return $result[0];
     }
 
